894a09151c3fb531ac3b255d97f78de8129c5336
Kanav Gupta <kanav0610@gmail.com>
2021-06-11 14:04:14 +0530

    fix rebuilding

--- build.py
+++ build.py
@@ -15,17 +15,21 @@ def main():
 		pass
 	os.chdir('build')
 
-	for filename in os.listdir('../src'):
-		if filename.endswith('.js'):
-			os.mkdir(filename[:-3])
-			copyfile('../index.rsh', filename[:-3] + '/index.rsh')
-			copyfile('../src/' + filename, filename[:-3] + '/bountyFunction.rsh')
-			os.chdir(filename[:-3])
-			os.system('../../reach compile')
-			os.chdir('..')
-		else:
-			continue
+	pathname = sys.argv[1]
 
+	if not pathname.startswith("src/"):
+		return
+	
+	filename = os.path.basename(pathname)
+	if filename.endswith('.js'):
+		os.mkdir(filename[:-3])
+		copyfile('../index.rsh', filename[:-3] + '/index.rsh')
+		copyfile('../src/' + filename, filename[:-3] + '/bountyFunction.rsh')
+		os.chdir(filename[:-3])
+		os.system('../../reach compile')
+		os.chdir('..')
+
+	os.chdir('..')
 	return
 
 if __name__ == '__main__':
